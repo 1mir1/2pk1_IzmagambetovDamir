b72482a5390a05895cff3b90698e9e6c3e44093f
Damir Izmagambetov <izmagambetovdamir4@gmail.com>
2022-09-22 17:45:03 +0500

    pz5

--- pz5/Program.cs
+++ pz5/Program.cs
@@ -10,7 +10,41 @@ namespace pz5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Работа с Git");
+            //Console.WriteLine("Первое задание");
+            //for (int i = 20; i <= 80; i += 5) 
+            //     Console.WriteLine(i);
+
+
+            //Console.WriteLine("Второе задание");
+            //for (char i = 'G'; i <= 'L'; i++)  
+            //    Console.Write(i);
+            //{
+            //Console.WriteLine();
+            //}
+
+
+            //Console.WriteLine("Третье задание");
+            //int n = 5;
+            //int m = 6; 
+            //for (int i = 0; i < m; i++)
+            //{
+            //    for (int x = 0; x < n; x++)
+            //    {
+            //        Console.Write('#');
+            //    }
+            //    Console.WriteLine();
+            //}
+
+
+            //Console.WriteLine("Четвертое задание");
+            //for (int i = 0; i <= 100; i++)
+            //{
+            //    if (i % 6 == 0)
+            //    {
+            //        Console.WriteLine(i + " ");
+            //    }
+            //}
         }
+
     }
 }
